01df181ae7a67441d324bee473ec9dcf3d413357
Nathan James <nathanjms@outlook.com>
2022-10-22 16:55:46 +0100

    Begin update and new note methods

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,35 @@
     >
       <!-- Sidebar -->
       <div class="h-0 overflow-auto flex-grow">
+        <div class="flex items-center h-8 px-3 group mt-4">
+          <div class="flex items-center flex-grow focus:outline-none">
+            <a
+              class="ml-2 leading-none font-medium text-sm"
+              href="#"
+              @click.prevent="showAllNotes"
+              >All Notes</a
+            >
+          </div>
+          <button @click="addNewNote">
+            <svg
+              class="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </button>
+        </div>
         <div class="mt-4">
           <a
+            @click.prevent="openNote(note)"
             class="flex items-center h-8 text-sm pl-8 pr-3"
             href="#"
             v-for="note in notes"
@@ -49,6 +76,7 @@ export default {
       editor: null,
       database: null,
       notes: [],
+      activeNote: {},
     };
   },
   async created() {
@@ -94,6 +122,9 @@ export default {
       });
     },
     async saveNote() {
+      if (Object.keys(this.activeNote).length) {
+        console.log("update instead");
+      }
       return new Promise((resolve) => {
         let transaction = this.database.transaction("notes", "readwrite");
         transaction.oncomplete = () => {
@@ -122,6 +153,14 @@ export default {
           resolve(e.target.result);
         };
       });
+    },
+    openNote(note) {
+      this.activeNote = note;
+      this.editor.commands.setContent(note.content);
+    },
+    addNewNote() {
+      this.activeNote = {};
+      this.editor.commands.setContent("");
     },
   },
 };
