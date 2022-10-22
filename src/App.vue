ec9bfc7a6ed03bba4f57aa19b65f6c5eb2a9fe1e
Nathan James <nathanjms@outlook.com>
2022-10-22 14:41:08 +0100

    Save note

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,33 @@
       class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100"
     >
       <!-- Sidebar -->
+      <div class="h-0 overflow-auto flex-grow">
+        <div class="mt-4">
+          <a
+            class="flex items-center h-8 text-sm pl-8 pr-3"
+            href="#"
+            v-for="note in notes"
+            :key="note.created"
+          >
+            <span class="ml-2 leading-none">{{
+              new Date(note.created).toLocaleString()
+            }}</span>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="flex flex-col flex-grow">
       <!-- Main Content -->
       <div class="flex flex-col flex-grow overflow-auto">
         <editor-content :editor="editor" />
+      </div>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button
+          @click="saveNote"
+          class="p-2 m-2 rounded-lg bg-gray-200 hover:bg-slate-400"
+        >
+          Save Note
+        </button>
       </div>
     </div>
   </div>
@@ -26,10 +48,13 @@ export default {
     return {
       editor: null,
       database: null,
+      notes: [],
     };
   },
   async created() {
     this.database = await this.getDatabase();
+    let notes = await this.getNotes();
+    this.notes = notes.reverse();
   },
   mounted() {
     this.editor = new Editor({
@@ -37,7 +62,7 @@ export default {
       extensions: [StarterKit],
       editorProps: {
         attributes: {
-          class: "prose my-6 px-3 mx-auto focus:outline-none",
+          class: "prose my-6 mx-auto focus:outline-none",
         },
       },
     });
@@ -48,18 +73,53 @@ export default {
   methods: {
     async getDatabase() {
       return new Promise((resolve, reject) => {
-        let db = window.indexedDB.open("notes");
+        let db = window.indexedDB.open("notes", 2);
+
         db.onerror = () => {
-          reject("Error opening the database");
+          reject("Error opening the database.");
         };
+
         db.onsuccess = (e) => {
-          console.log("onsuccess", e);
+          console.log("db.onsuccess", e);
           resolve(e.target.result);
         };
+
         db.onupgradeneeded = (e) => {
-          // Pass in/provide schema changes.
-          console.log("onupgradeneeded", e);
-          e.target.result.createObjectStore("notes");
+          console.log("db.onupgradeneeded", e);
+          if (e.target.result.objectStoreNames.contains("notes")) {
+            e.target.result.deleteObjectStore("notes");
+          }
+          e.target.result.createObjectStore("notes", { keyPath: "created" });
+        };
+      });
+    },
+    async saveNote() {
+      return new Promise((resolve) => {
+        let transaction = this.database.transaction("notes", "readwrite");
+        transaction.oncomplete = () => {
+          resolve();
+          console.log("resolve");
+        };
+
+        console.log("create");
+        let now = new Date();
+        let note = {
+          content: this.editor.getHTML(),
+          created: now.getTime(),
+        };
+
+        this.notes.unshift(note);
+        transaction.objectStore("notes").add(note);
+      });
+    },
+    async getNotes() {
+      return new Promise((resolve) => {
+        this.database
+          .transaction("notes")
+          .objectStore("notes")
+          .getAll().onsuccess = (e) => {
+          console.log("getNotes()", e.target.result);
+          resolve(e.target.result);
         };
       });
     },
